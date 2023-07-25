2975b7c00bafccab0f1d2b2166b4fd89299b8e66
dungngoo <ngodung06vn@gmail.com>
2023-07-25 11:01:44 +0700

    Áp dụng PropTypes vào dự án TikTok

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,17 +1,22 @@
-import Button from "~/components/Button";
-import styles from "./Menu.module.scss";
-import classNames from "classnames/bind";
-
+import Button from '~/components/Button';
+import styles from './Menu.module.scss';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 const cx = classNames.bind(styles);
 function MenuItem({ data, onClick }) {
-  const classes = cx("menu-item", {
-    separate: data.separate,
-  });
-  return (
-    <Button className={classes} to={data.to} leftIcon={data.icon} onClick={onClick}>
-      {data.title}
-    </Button>
-  );
+    const classes = cx('menu-item', {
+        separate: data.separate,
+    });
+    return (
+        <Button className={classes} to={data.to} leftIcon={data.icon} onClick={onClick}>
+            {data.title}
+        </Button>
+    );
 }
+
+MenuItem.propTypes = {
+    data: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
+};
 
 export default MenuItem;
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,5 +1,6 @@
 import styles from './Menu.module.scss';
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import MenuItem from './MenuItem';
 import Tippy from '@tippyjs/react/headless';
@@ -47,7 +48,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                     <PopperWrapper className={cx('menu-popper')}>
                         {history.length > 1 && (
                             <Header
-                                title="Language"
+                                title={current.title}
                                 onBack={() => {
                                     setHistory((prev) => prev.slice(0, prev.length - 1));
                                 }}
@@ -65,5 +66,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         </Tippy>
     );
 }
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
 
 export default Menu;
